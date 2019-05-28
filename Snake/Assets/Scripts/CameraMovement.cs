086974e59a6d7d299fc2a55f6dc20f8135611c6b
Vincent Lacoste <35927607+Zmex14@users.noreply.github.com>
2019-05-28 10:59:02 +0200

    Ajout de : - Menu principal - Audio Manager (voir script AudioManager.cs pour savoir où placer les sons dans les scripts) - Ebauche d'une caméra qui se déplace aléatoirement

---
+++ Snake/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Transform target; //Position de la cible
+
+    public float cameraSpeed = 25f;
+
+    public float timerNextMoveIni; //Le temps qu'il faut à la caméra avant le prochain déplacement
+    private float timerNextMove;
+
+    public float timerMinValue = 10f; //Temps minimum avant que la caméra se déplace
+    public float timerMaxValue = 30f; //Temps maximum avant que la caméra se déplace
+
+    private bool needATarget = false;
+
+    public GameObject[] wayPoint;
+    public int index;
+    public int lastPointReached;
+
+    public GameObject test;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPoint = GameObject.FindGameObjectsWithTag("Waypoint");
+        timerNextMoveIni = Random.Range(timerMinValue, timerMaxValue); //Pour équilibrage GD
+        timerNextMove = timerNextMoveIni;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerNextMove > 0)
+        {
+            timerNextMove -= Time.deltaTime;
+        }
+
+        if (timerNextMove <= 0 && needATarget == false)
+        {
+            GetNextWaypoint();
+        }
+
+        if (needATarget == true)
+        {
+            GoToNextPoint();
+        }
+
+    }
+
+    public void GetNextWaypoint()
+    {
+        index = Random.Range(0, wayPoint.Length);
+        if (index != lastPointReached)
+        {
+            lastPointReached = index;
+            target = wayPoint[index].transform;
+            Debug.Log("Next point is " + target.name);
+            needATarget = true;
+        }
+        else
+        {
+            GetNextWaypoint();
+        }
+
+    }
+
+    public void GoToNextPoint()
+    {
+        Debug.Log("Going to " + target.name);
+        Vector3 dir = Vector3.Slerp(test.transform.position, target.position - test.transform.position, 5); 
+        test.transform.Translate(dir.normalized * cameraSpeed * Time.deltaTime, Space.World); 
+
+        if (Vector3.Distance(test.transform.position, target.position) <= 0.4f) //Valeur moyenne car déplacements imprécis.
+        {
+            timerNextMove = timerNextMoveIni;
+            needATarget = false;
+            Debug.Log("Point Reached !");
+        }
+    }
+
+
+}
---
+++ Snake/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioClip soundSelection;
+    public AudioClip music;
+    private AudioSource audioSource;
+    public GameObject FirstSelectedObject;
+    public EventSystem EventSystem;
+
+
+    void Start()
+    {
+        audioSource.PlayOneShot(music, 1f);
+
+        if (EventSystem.currentSelectedGameObject == null)
+        {
+            EventSystem.SetSelectedGameObject(FirstSelectedObject);
+        }
+    }
+
+    public void PlayGame()
+    {
+        audioSource.PlayOneShot(soundSelection, 1f);
+        SceneManager.LoadScene("Scene_Vincent"); //***** A changer *****//
+    }
+
+    public void QuitGame()
+    {
+        audioSource.PlayOneShot(soundSelection, 1f);
+        Application.Quit();
+    }
+}
